e0560a7bade1a50042eeb331ac650e64f8391ac9
Pavel <17pavel7@gmail.com>
2024-07-20 20:06:41 +0300

    hotels.py

--- api/api/hotels.py
+++ api/api/hotels.py
@@ -1,4 +1,3 @@
-
 from fastapi import APIRouter, Query, Depends
 from typing import Optional
 from datetime import date
@@ -6,53 +5,52 @@ from pydantic import BaseModel
 
 router = APIRouter()
 
+
 class SBooking(BaseModel):
     room_id: int
-    date_from: date 
+    date_from: date
     date_to: date
     stars: Optional[int] = Query(None, ge=1, le=5)
-    
-
 
 class SHotel(BaseModel):
     address: str
     name: str
-    stars: int   
+    stars: int
+
 
 class HotelsArgs:
+
     def __init__(
         self,
         location: str,
-        date_from: date, 
+        date_from: date,
         date_to: date,
         stars: Optional[int] = Query(None, ge=1, le=5),
-        has_spa: Optional[bool] = None
-        ):
+        has_spa: Optional[bool] = None,
+    ):
         self.location = location
         self.date_from = date_from
         self.date_to = date_to
         self.stars = stars
         self.has_spa = has_spa
-        
+
 
 @router.get("/hotels")
-def get_hotels(search_args: HotelsArgs=Depends())->list[SHotel]:
+def get_hotels(search_args: HotelsArgs = Depends()) -> list[SHotel]:
     hotels = [
         {
             "address": "kemer",
             "name": "kylykia",
             "stars": 5,
-            
         },
         {
             "address": "kemer",
             "name": "meder",
             "stars": 4,
-            
-        }
+        },
     ]
     return hotels
-    
+
 
 @router.post("/bookings")
 def add_bookings(booking: SBooking):
--- api/main.py
+++ api/main.py
@@ -3,9 +3,7 @@ import uvicorn
 from api.hotels import router as router_hotels
 
 app = FastAPI()
-
 app.include_router(router_hotels)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
-
